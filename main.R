571490cfd4dbba1de0a64aa19e1b4d5ec0c56b20
aroge23 <42726350+aroge23@users.noreply.github.com>
2022-02-22 19:22:48 -0500

    initial backtest implementation

--- main.R
+++ main.R
@@ -13,7 +13,7 @@ getSymbols(stock_index, verbose = TRUE, src = "yahoo",
 #Variables
 
 #Expiration
-EXP = as.Date("2022-02-04")
+EXP = as.Date("2022-03-18")
 
 #Today's date
 TODAY = Sys.Date()
@@ -26,7 +26,7 @@ STK = tail(GOOG[, c(4)], n = 1)
 STK = coredata(STK)
 
 #Strike price - AUTOMATE
-STRIKE = 2665
+STRIKE = 2605
 
 #Risk free rate of return
 getSymbols.FRED("DGS10", env = .GlobalEnv)
@@ -37,12 +37,71 @@ Rf = sum(rep(DGS10, days2exp))
 DIV = 0.00
 
 #Volatility - AUTOMATE VALUE
-VOL = as.numeric(AmericanOptionImpliedVolatility(type= "call", value= 120, strike= STRIKE,
+VOL = as.numeric(AmericanOptionImpliedVolatility(type= "call", value= 95.93, strike= STRIKE,
                                                  underlying = STK, dividendYield = DIV, 
-                                                 riskFreeRate = Rf, maturity = days2exp/365, volatility = 0.5))
+                                                 riskFreeRate = 0.1, maturity = days2exp/365, volatility = 0.5))
 
-binomopt(s= STK, k= STRIKE, v=VOL, r= Rf, tt= 10/365, d= DIV, nstep= 1,
+#greeks
+greeks(bscall(s=STK, k=STRIKE, v=VOL, r=Rf, tt= 30/365, d=DIV), complete= TRUE)
+
+binomopt(s= STK, k= STRIKE, v=VOL, r= Rf, tt= days2exp/365, d= DIV, nstep= 1,
              putopt= FALSE, american= TRUE, returntrees= TRUE)
 
-binomplot(s= STK, k= STRIKE, v=VOL, r= Rf, tt= 10/365, d= DIV, nstep= 1,
+binomplot(s= STK, k= STRIKE, v=VOL, r= Rf, tt= days2exp/365, d= DIV, nstep= 1,
           putopt= FALSE, american= TRUE, plotarrows= TRUE, plotvalues= TRUE)
+
+
+#------------------------------------------------------------
+DATES = seq.Date(from=as.Date("2021-10-16"), to=Sys.Date() + 30, by= "1 day")
+NAxts = xts(rep(NA, length(DATES)), order.by= DATES)
+DATES = DATES[options.expiry(NAxts)]
+
+callStrat = function(expiry_dates, symbol, strikesAboveATM) {
+  #pass in the expirations and extract options for ticker
+  ops = lapply(as.list(2:length(expiry_dates)), function(ii){
+    # assign open_date & next_expiry (1-month expirations)
+    open_date = expiry_dates[ii - 1]
+    next_expiry = expiry_dates[ii]
+    
+    #read in options
+    op = 1#FUNCTION FOR STRIKE VS PRICE DATA
+    
+    #get the closest strike to the last traded stock price
+    op$stkPrc2strike = as.numeric(op$stkClose) - as.numeric(op$strike)
+    if(strikesAboveATM == 0) {
+      #eliminate over the money calls
+      op = subset(op, op$stkPrc2strike > 0)
+      #which has the least difference to the stock close
+      ATM = op[which.min(op$stkPrc2strike),]
+    } else {
+      #if strikesAboveATM is above max strikes available, last strike will be selected:
+      if (strikesAboveATM > length(op$stkPrc2strike)) {
+        strikesAboveATM = length(op$stkPrc2strike)
+      }
+      #find the OTM calls and select
+      ATM = op[which(op$stkPrc2strike < 0)[strikesAboveATM],]
+    }
+    #extract desired columns
+    ATM = ATM[, c("Date", "expiry", "days2Exp", "stkClose", "strike", "Mid")]
+    colnames(ATM) = c("openDate", "expiry", "days2exp", "open_stkPrc", "strike", "call_premium")
+    ATM
+  })
+  #combine options
+  ops = rbindlist(ops, use.names= TRUE, fill= TRUE)
+  #get the latest quote for the last expiration
+  lastPrc = getQuote(symbol)$Last
+  #add expiration prices to ops
+  ops$prcAtExp = c(ops$open_stkPrc[2:nrow(ops)], lastPrc)
+  #calculate the net premium received
+  ops$stk2strike = (as.numeric(ops$prcAtExp) - as.numeric(ops$strike))
+  #ops$netPremium = ifelse(ops$stk2strike > 0, as.numeric(ops$call_premium), as.numeric(ops$call_premium) + ops$stk2strike)
+  ops$netPremium = ifelse(ops$stk2strike > 0, (as.numeric(ops$strike) - as.numeric(ops$open_stkPrc))
+                          + as.numeric(ops$call_premium), as.numeric(ops$call_premium) + ops$stk2strike)
+  
+  #calculate returns
+  ops$ccRet = round(ops$netPremium / ops$open_stkPrc, 4)
+  ops$stkRet = round(ops$prcAtexp / ops$open_stkPrc - 1, 4)
+  
+  #return the results
+  ops
+}
